cb9436e7b39bb2f916cbd3c9f70c636dda15946d
Krishna080492 <krishna.desai4@gmail.com>
2024-10-10 19:09:31 +0530

    Day-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './containers/Home'
+import Contact from './containers/Contact'
+import Blog from './containers/Blog'
+import Service from './containers/Service'
+import About from './containers/About'
+import Shop from './containers/Shop'
 
 function App() {
 
@@ -10,7 +16,12 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route />
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,24 +7,24 @@ function Header() {
       {/* Start Header/Navigation */}
       <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
         <div className="container">
-          <Link className="navbar-brand" href="index.html">Furni<span>.</span></Link>
+          <Link className="navbar-brand" to="index">Furni<span>.</span></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarsFurni">
             <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
               <li className="nav-item active">
-                <NavLink className="nav-link" href="index.html">Home</NavLink>
+                <NavLink className="nav-link" to="/">Home</NavLink>
               </li>
-              <li><NavLink className="nav-link" href="shop.html">Shop</NavLink> </li>
-              <li><NavLink className="nav-link" href="about.html">About us</NavLink> </li>
-              <li><NavLink className="nav-link" href="services.html">Services</NavLink> </li>
-              <li><NavLink className="nav-link" href="blog.html">Blog</NavLink> </li>
-              <li><NavLink className="nav-link" href="contact.html">Contact us</NavLink> </li>
+              <li><NavLink className="nav-link" to="/shop">Shop</NavLink> </li>
+              <li><NavLink className="nav-link" to="/about">About us</NavLink> </li>
+              <li><NavLink className="nav-link" to="/services">Services</NavLink> </li>
+              <li><NavLink className="nav-link" to="/blog">Blog</NavLink> </li>
+              <li><NavLink className="nav-link" to="/contact">Contact us</NavLink> </li>
             </ul>
             <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-              <li><NavLink className="nav-link" href="#"><img src="images/user.svg" /></NavLink> </li>
-              <li><NavLink className="nav-link" href="cart.html"><img src="images/cart.svg" /></NavLink> </li>
+              <li><NavLink className="nav-link" to="#"><img src="images/user.svg" /></NavLink> </li>
+              <li><NavLink className="nav-link" to="cart"><img src="images/cart.svg" /></NavLink> </li>
             </ul>
           </div>
         </div>
